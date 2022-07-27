27171cb65988dd81cf207ed486e3667685e61a94
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-07-27 13:35:49 -0300

    finishing app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://www.youtube.com/watch?v=2K6ZyZ2XZnY
+https://www.youtube.com/watch?v=tBr-PybP_9c&t=784s
--- server/server.js
+++ server/server.js
@@ -1,60 +1,73 @@
-const { nanoid } = require("nanoid")
-const socketIO = require('socket.io')
-const app = require('express')()
-const server = require('http').createServer(app)
+const app = require("express")();
+const server = require("http").createServer(app);
+const PORT = 5000;
+const { nanoid } = require("nanoid");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const PORT = 3000
-const io = socketIO(server, {
-    cors: {
-        origin: "*"
-    },
-})
+let users = [];
+let rooms = [];
 
-const users = []
-const rooms = []
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
 
-io.on('connection', socket => {
+  socket.emit("me", socket.id);
+  users.push(socket.id);
 
-    // users
-    socket.emit('me', socket.id)
-    users.push(socket.id)
-    
-    socket.broadcast.emit('updateUsers', users)
-    socket.on('disconnect', () => {
-        users = users.filter(user => user !== socket.id)
-        socket.broadcast.emit('updateUsers', users)
-        socket.disconnect()
-    })
-    
-    socket.emit('getAllUsers', users)
+  socket.broadcast.emit("updateUsers", users);
 
-    
-    // rooms
-    socket.emit('create_room', () => {
-        const room = {
-            id: nanoid(7),
-            chat: []
-        }
-        socket.join(room)
-        socket.emit('get_room', room)
-        rooms.push(room)
-        socket.broadcast.emit('updateRooms', rooms)
-    })
-    socket.on('join_room', (room) => {
-        socket.join(room.id)
-    })
-    socket.broadcast.emit('updateRooms', rooms)
-    socket.on('message', payload => {
-        rooms.map(room => {
-            if(room.id == payload.room) {
-                singleChat = { message: payload.message, writer:payload.socketId }
-                room.chat.push(singleChat)
-            }
-        })
-        io.to(payload.room).emit('chat', payload)
-    })
-})
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected.`);
+    users = users.filter((user) => user !== socket.id);
+    console.log(users);
+    socket.broadcast.emit("updateUsers", users);
+    socket.disconnect();
+  });
 
-server.listen(PORT, (req, res) => {
-    console.log('server is running on port 3000')
-})
+  socket.emit("getAllUsers", users);
+  console.log(users);
+
+  // Rooms
+  socket.on("create_room", () => {
+    const room = {
+      id: nanoid(7),
+      capacity: 10,
+      usersJoined: [socket.id],
+      chat: [],
+    };
+    socket.join(room);
+    socket.emit("get_room", room);
+    console.log("Room created: " + room.id);
+    rooms.push(room);
+
+    socket.broadcast.emit("updateRooms", rooms);
+  });
+
+  socket.on("join_room", (room) => {
+    socket.join(room.id);
+    console.log(`user ${socket.id} joined room: ${room.id}`);
+  });
+  socket.emit("getAllRooms", rooms);
+
+  socket.broadcast.emit("updateRooms", rooms);
+
+  socket.on("message", (payload) => {
+    console.log(`Message from ${socket.id} : ${payload.message}`);
+    rooms.map((room) => {
+      if (room.id === payload.room) {
+        singleChat = { message: payload.message, writer: payload.socketId };
+        room.chat.push(singleChat);
+        payload.chat = room.chat;
+      }
+    });
+
+    io.to(payload.room).emit("chat", payload);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
